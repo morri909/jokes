bcefcce750273992372dcbba9f241824a8e48cc7
Michael Morrison <morri909@umn.edu>
2021-01-16 12:35:12 -0600

    Rename controller

--- src/main/java/com/spydrone/jokes/controllers/JokeController.java
+++ src/main/java/com/spydrone/jokes/controllers/JokeController.java
@@ -6,11 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class JokesController {
+public class JokeController {
 
 	private final JokeService jokeService;
 
-	public JokesController(JokeService jokeService) {
+	public JokeController(JokeService jokeService) {
 		this.jokeService = jokeService;
 	}
 
