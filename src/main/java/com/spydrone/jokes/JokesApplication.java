f1499edb4b01baa768c9af5b4d65086f71689acb
Michael Morrison <morri909@umn.edu>
2021-01-19 19:23:54 -0600

    XML bean configuration example

--- src/main/java/com/spydrone/jokes/JokesApplication.java
+++ src/main/java/com/spydrone/jokes/JokesApplication.java
@@ -2,8 +2,10 @@ package com.spydrone.jokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesApplication {
 
 	public static void main(String[] args) {
